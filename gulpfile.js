f73b5e716469edf69f3692801874d3462562884d
DJYoko <yokochan.recurrent@gmail.com>
2020-04-27 00:51:55 +0900

    format gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
 
-gulp.task('sass', function () {
-    return (gulp.src('./src/css/*.scss')
-        .pipe(sass({ outputStyle: 'expanded' }))
-        .pipe(gulp.dest('./docs/css/')));
+gulp.task("sass", function () {
+  return gulp
+    .src("./src/css/*.scss")
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("./docs/css/"));
 });
 
-gulp.task('default', function () {
-
-    return (gulp.watch(
-        './src/css/*.scss',
-        gulp.series('sass')));
-
+gulp.task("default", function () {
+  return gulp.watch("./src/css/*.scss", gulp.series("sass"));
 });
